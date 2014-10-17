e21bf9202f00c4ec90dad066fa31afc2e1c4ae3c
Cort <cort@Corts-iMac.local>
2014-10-17 00:48:51 -0500

    Remove menu bar on OS X.

--- .emacs.d/core/prelude-osx.el
+++ .emacs.d/core/prelude-osx.el
@@ -1,37 +1,3 @@
-;;; prelude-osx.el --- Emacs Prelude: OSX specific settings.
-;;
-;; Copyright © 2011-2013 Bozhidar Batsov
-;;
-;; Author: Bozhidar Batsov <bozhidar@batsov.com>
-;; URL: https://github.com/bbatsov/prelude
-;; Version: 1.0.0
-;; Keywords: convenience
-
-;; This file is not part of GNU Emacs.
-
-;;; Commentary:
-
-;; Some OSX specific stuff.
-
-;;; License:
-
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 3
-;; of the License, or (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
-
-;;; Code:
-
 ;; On OS X Emacs doesn't use the shell PATH if it's not started from
 ;; the shell. Let's fix that:
 (prelude-require-packages '(exec-path-from-shell vkill))
@@ -63,8 +29,6 @@ Windows external keyboard from time to time."
 
 (define-key prelude-mode-map (kbd "C-c w") 'prelude-swap-meta-and-super)
 (define-key prelude-mode-map (kbd "s-/") 'hippie-expand)
-
-(menu-bar-mode +1)
 
 (provide 'prelude-osx)
 ;;; prelude-osx.el ends here
