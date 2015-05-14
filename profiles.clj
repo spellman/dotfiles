e230809ff26470067e6ca1059c2ba691a3227d5d
Cort Spellman <spellman.cort@gmail.com>
2015-05-13 22:01:08 -0500

    profiles.clj: Update tools.nrepl dependency ver.
    Update version from 0.2.7 to 0.2.10
    This shouldn't be necessary; there is a bug filed. Remove this
    dependency when possible.

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-try  "0.4.3"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+                  [lein-try "0.4.3"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
