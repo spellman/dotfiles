e1c52759409a3ba2f422d57dc4fbf5553354cbd1
Cort Spellman <spellman.cort@gmail.com>
2015-07-16 00:55:48 -0500

    profiles.clj Update cider-nrepl version.
    Note that
    1) I shouldn't have to do this at all. There is a bug filed, as per a
    previous commit message.
    2) I'm using a stable release and not a snapshot. The cider maintainer,
    Bozhidar Batsov, said on the Cognicast podcast that they are adding
    debug functionality and they're breaking things so it would be smart to
    stick with releases from 0.9 to 1.0.

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]
                   [lein-try "0.4.3"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
