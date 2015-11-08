2cfe06490c368ce40e8d3bbf0bc7adae30fa828e
Cort Spellman <spellman.cort@gmail.com>
2015-11-08 03:18:31 -0600

    .lein/profiles.clj: Update cider-nrepl, add alembic.
    Update cider-nrepl for the cider version Spacemacs uses.
    Add alembic dependency to allow adding depdencies to a running REPL.
    See https://github.com/pallet/alembic

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+{:user {:plugins [[cider/cider-nrepl  "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "1.2.0-SNAPSHOT"]
+                  [lein-pprint "1.1.1"]
                   [lein-try "0.4.3"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.10"]]}}
