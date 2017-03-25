66077df927e90581eead08dfcaf56e965bdc1785
Cort Spellman <spellman.cort@gmail.com>
2017-03-25 16:02:31 -0500

    Remove old modified Prelude emacs setup.

--- .emacs.d.my-modified-prelude/modules/prelude-projectile.el
+++
@@ -1,8 +0,0 @@
-(prelude-require-package 'projectile)
-
-;; Moved from core/prelude-editor.el
-(require 'projectile)
-(setq projectile-cache-file (expand-file-name  "projectile.cache" prelude-savefile-dir))
-(projectile-global-mode t)
-
-(provide 'prelude-projectile)
--- .emacs.d.my-modified-prelude/personal/settings.el
+++
@@ -1,2 +0,0 @@
-;; Disable spellchecking.
-(setq prelude-flyspell nil)
