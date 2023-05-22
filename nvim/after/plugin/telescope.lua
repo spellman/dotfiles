13b5517e11e1c69193bfc0e455a79ce7f0437748
Cort Spellman <spellman.cort@gmail.com>
2023-05-22 12:38:39 -0500

    nvim: Format telescope normal display ~ like Astronvim
    Prompt at top, list goes down.
    
    I don't know if I have this just like Astronvim yet. They have it set up
    pretty nicely. But I copied these configuration options from Astronvim.

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -7,6 +7,20 @@ require("telescope").setup({
         ["<C-d>"] = false,
       },
     },
+    path_display = { "truncate" },
+    sorting_strategy = "ascending",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
   },
 })
 
