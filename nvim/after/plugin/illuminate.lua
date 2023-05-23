f62436ae241e0d93c36b1ccc9e24f9efd6e8fdb1
Cort Spellman <spellman.cort@gmail.com>
2023-05-22 21:08:08 -0500

    nvim: Configure highlighting occurrences of word under cursor
    Highlight occurrences of word under cursor as though they were
    highlighted in visual mode -- this should make the highlighting
    theme-independent. This as opposed to manually selecting background
    colors.
    
    Don't highlight the word under the cursor, itself.
    
    Don't highlight in quickfix, location list, or Telescope buffers.

--- nvim/after/plugin/illuminate.lua
+++ nvim/after/plugin/illuminate.lua
@@ -1,9 +1,31 @@
 require("illuminate").configure({
   delay = 50,
+  filetypes_denylist = {
+    "qf", -- quickfix and location list buffers
+    "TelescopePrompt"
+  },
+  under_cursor = false,
 })
 
+-- Manual background settings for onedark theme.
 -- vim.cmd([[
 -- highlight IlluminatedWordText guifg=none guibg=#484858 gui=none
 -- highlight IlluminatedWordRead guifg=none guibg=#484858 gui=none
 -- highlight IlluminatedWordWrite guifg=none guibg=#484858 gui=none
 -- ]])
+
+-- Source: https://ansidev.xyz/posts/2023-04-25-how-to-change-the-highlight-style-using-vim-illuminate
+-- change the highlight style
+vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+
+--- auto update the highlight style on colorscheme change
+vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+  pattern = { "*" },
+  callback = function(ev)
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+  end
+})
