13d17861ac640f9a2aa024015d311c99a1793b12
Cort Spellman <spellman.cort@gmail.com>
2023-08-15 20:53:45 -0500

    nvim: Configure Neogit, although it doesn't totally work
    https://github.com/NeogitOrg/neogit#disabling-insert-on-commit
    I have `disable_insert_on_commit set = false` but it does nothing.

--- nvim/after/plugin/neogit.lua
+++ nvim/after/plugin/neogit.lua
@@ -1,3 +1,55 @@
 local neogit = require("neogit")
 
-neogit.setup({})
+neogit.setup({
+  commit_popup = {
+    kind = "split_above",
+  },
+  disable_hint = true,
+  disable_insert_on_commit = false,
+
+  -- Setting any section to `false` will make the section not render at all
+  sections = {
+    rebase = {
+      folded = false
+    },
+    recent = {
+      folded = false
+    },
+    staged = {
+      folded = false
+    },
+    stashes = {
+      folded = false
+    },
+    unmerged = {
+      folded = false
+    },
+    unmerged_pushRemote = {
+      folded = false
+    },
+    unmerged_upstream = {
+      folded = false
+    },
+    unpulled = {
+      folded = false
+    },
+    unpulled_pushRemote = {
+      folded = false
+    },
+    unpulled_upstream = {
+      folded = false
+    },
+    unstaged = {
+      folded = false
+    },
+    untracked = {
+      folded = false
+    },
+  },
+  signs = {
+    -- { CLOSED, OPENED }
+    section = { ">", "v" },
+    item = { ">", "v" },
+    hunk = { ">", "v" },
+  },
+})
