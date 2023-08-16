a8e22255ce484c2df7160ec046142b85ef125810
Cort Spellman <spellman.cort@gmail.com>
2023-08-15 20:45:20 -0500

    nvim: Prioritize space in statusbar

---
+++ nvim/after/plugin/lualine.lua
@@ -0,0 +1,44 @@
+require("lualine").setup({
+  options = {
+    disabled_filetypes = {
+      statusline = {
+        "aerial",
+        "qf", -- quickfix and location list buffers
+        "NeogitCommitView",
+        "NeogitStatus",
+        "TelescopePrompt",
+        "undotree",
+      },
+      winbar = {
+        "aerial",
+        "qf", -- quickfix and location list buffers
+        "NeogitCommitView",
+        "NeogitStatus",
+        "TelescopePrompt",
+        "undotree",
+      },
+    },
+    icons_enabled = true,
+    theme = "auto",
+    component_separators = "|",
+    section_separators = "|",
+    -- always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = {"filename"},
+    lualine_b = {"location", "progress"},
+    lualine_c = {"filetype"},
+    lualine_x = {"branch"},
+    lualine_y = {"diagnostics"},
+    lualine_z = {"mode"},
+  },
+  inactive_sections = {
+    lualine_a = {"filename"},
+    lualine_b = {"location"},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  }
+})
+
--- nvim/lua/cort/plugins.lua
+++ nvim/lua/cort/plugins.lua
@@ -155,19 +155,7 @@ return {
   --   end,
   -- },
 
-  {
-    -- Set lualine as statusline
-    "nvim-lualine/lualine.nvim",
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = "onelight",
-        component_separators = "|",
-        section_separators = "",
-      },
-    },
-  },
+  "nvim-lualine/lualine.nvim",
 
   -- "gc" to comment visual regions/lines
   {
@@ -377,8 +365,6 @@ return {
       "SmiteshP/nvim-navic",
       "nvim-tree/nvim-web-devicons", -- optional dependency
     },
-    opts = {
-      -- configurations go here
-    },
+    opts = {},
   }
 }
