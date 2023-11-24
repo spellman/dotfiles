2ac81a74d1e3f7f8f2cb2d4d33e6f6dae5665360
Cort Spellman <spellman.cort@gmail.com>
2023-11-24 12:49:07 -0600

    nvim: Customize NeoVim colors in Catppuccin Latte theme

--- .config/nvim/after/plugin/catppuccin.lua
+++ .config/nvim/after/plugin/catppuccin.lua
@@ -34,7 +34,251 @@ require("catppuccin").setup({
       base = "#ffffff",
       mantle = "#f0f0f0",
       crust = "#e4e4e4",
-    }
+    },
+  },
+  highlight_overrides = {
+    latte = function(colors)
+      local color_utils = require("catppuccin.utils.colors")
+      return {
+        NeogitBranch = {
+          fg = colors.peach,
+          style = { "bold" },
+        },
+        NeogitRemote = {
+          fg = colors.green,
+          style = { "bold" },
+        },
+        NeogitUnmergedInto = {
+          link = "Function",
+        },
+        NeogitUnpulledFrom = {
+          link = "Function",
+        },
+        NeogitObjectId = {
+          link = "Comment",
+        },
+        NeogitStash = {
+          link = "Comment",
+        },
+        NeogitRebaseDone = {
+          link = "Comment",
+        },
+        NeogitHunkHeader = {
+          bg = color_utils.darken(colors.blue, 0.095, colors.base),
+          fg = color_utils.darken(colors.blue, 0.500, colors.base),
+        },
+        NeogitHunkHeaderHighlight = {
+          bg = color_utils.darken(colors.blue, 0.215, colors.base),
+          fg = colors.blue,
+        },
+        NeogitDiffContextHighlight = {
+          bg = colors.base,
+        },
+        NeogitDiffDeleteHighlight = {
+          bg = color_utils.darken(colors.red, 0.125, colors.base),
+          fg = color_utils.lighten(colors.red, 0.850, colors.text),
+        },
+        NeogitDiffAddHighlight = {
+          bg = color_utils.darken(colors.green, 0.125, colors.base),
+          fg = color_utils.lighten(colors.green, 0.850, colors.text),
+        },
+        NeogitDiffContext = {
+          bg = colors.base,
+          fg = color_utils.lighten(colors.base, 0.180, colors.text),
+        },
+        NeogitDiffDelete = {
+          bg = color_utils.darken(colors.red, 0.050, colors.base),
+          fg = color_utils.darken(colors.red, 0.575, colors.base),
+        },
+        NeogitDiffAdd = {
+          bg = color_utils.darken(colors.green, 0.050, colors.base),
+          fg = color_utils.darken(colors.green, 0.575, colors.base),
+        },
+        NeogitCommitViewHeader = {
+          bg = color_utils.darken(colors.blue, 0.300, colors.base),
+          fg = color_utils.lighten(colors.blue, 0.800, colors.text),
+        },
+        NeogitChangeModified = {
+          fg = colors.blue,
+          style = { "bold" },
+        },
+        NeogitChangeDeleted = {
+          fg = colors.red,
+          style = { "bold" },
+        },
+        NeogitChangeAdded = {
+          fg = colors.green,
+          style = { "bold" },
+        },
+        NeogitChangeRenamed = {
+          fg = colors.mauve,
+          style = { "bold" },
+        },
+        NeogitChangeUpdated = {
+          fg = colors.peach,
+          style = { "bold" },
+        },
+        NeogitChangeCopied = {
+          fg = colors.pink,
+          style = { "bold" },
+        },
+        NeogitChangeBothModified = {
+          fg = colors.yellow,
+          style = { "bold" },
+        },
+        NeogitChangeNewFile = {
+          fg = colors.green,
+          style = { "bold" },
+        },
+        NeogitUntrackedfiles = {
+          fg = colors.mauve,
+          style = { "bold" },
+        },
+        NeogitUnstagedchanges = {
+          fg = colors.mauve,
+          style = { "bold" },
+        },
+        NeogitUnmergedchanges = {
+          fg = colors.mauve,
+          style = { "bold" },
+        },
+        NeogitUnpulledchanges = {
+          fg = colors.mauve,
+          style = { "bold" },
+        },
+        NeogitRecentcommits = {
+          fg = colors.mauve,
+          style = { "bold" },
+        },
+        NeogitStagedchanges = {
+          fg = colors.mauve,
+          style = { "bold" },
+        },
+        NeogitStashes = {
+          fg = colors.mauve,
+          style = { "bold" },
+        },
+        NeogitRebasing = {
+          fg = colors.mauve,
+          style = { "bold" },
+        },
+        NeogitNotificationInfo = {
+          fg = colors.blue,
+        },
+        NeogitNotificationWarning = {
+          fg = colors.yellow,
+        },
+        NeogitNotificationError = {
+          fg = colors.red,
+        },
+        NeogitGraphRed = {
+          fg = colors.red,
+        },
+        NeogitGraphWhite = {
+          fg = colors.base,
+        },
+        NeogitGraphYellow = {
+          fg = colors.yellow,
+        },
+        NeogitGraphGreen = {
+          fg = colors.green,
+        },
+        NeogitGraphCyan = {
+          fg = colors.blue,
+        },
+        NeogitGraphBlue = {
+          fg = colors.blue,
+        },
+        NeogitGraphPurple = {
+          fg = colors.lavender,
+        },
+        NeogitGraphGray = {
+          fg = colors.subtext1,
+        },
+        NeogitGraphOrange = {
+          fg = colors.peach,
+        },
+        NeogitGraphBoldRed = {
+          fg = colors.red,
+          style = { "bold" },
+        },
+        NeogitGraphBoldWhite = {
+          fg = colors.white,
+          style = { "bold" },
+        },
+        NeogitGraphBoldYellow = {
+          fg = colors.yellow,
+          style = { "bold" },
+        },
+        NeogitGraphBoldGreen = {
+          fg = colors.green,
+          style = { "bold" },
+        },
+        NeogitGraphBoldCyan = {
+          fg = colors.blue,
+          style = { "bold" },
+        },
+        NeogitGraphBoldBlue = {
+          fg = colors.blue,
+          style = { "bold" },
+        },
+        NeogitGraphBoldPurple = {
+          fg = colors.lavender,
+          style = { "bold" },
+        },
+        NeogitGraphBoldGray = {
+          fg = colors.subtext1,
+          style = { "bold" },
+        },
+        NeogitPopupBold = {
+          style = { "bold" },
+        },
+        NeogitPopupSwitchKey = {
+          fg = colors.lavender,
+        },
+        NeogitPopupOptionKey = {
+          fg = colors.lavender,
+        },
+        NeogitPopupConfigKey = {
+          fg = colors.lavender,
+        },
+        NeogitPopupActionKey = {
+          fg = colors.lavender,
+        },
+        NeogitFilePath = {
+          fg = colors.blue,
+          style = { "italic" },
+        },
+        NeogitDiffHeader = {
+          bg = colors.base,
+          fg = colors.blue,
+          style = { "bold" },
+        },
+        NeogitDiffHeaderHighlight = {
+          bg = colors.base,
+          fg = colors.peach,
+          style = { "bold" },
+        },
+        NeogitUnpushedTo = {
+          fg = colors.lavender,
+          style = { "bold" },
+        },
+        NeogitFold = {
+          fg = colors.none,
+          bg = colors.none,
+        },
+        NeogitSectionHeader = {
+          fg = colors.mauve,
+          style = { "bold" },
+        },
+        NeogitTagName = {
+          fg = colors.yellow,
+        },
+        NeogitTagDistance = {
+          fg = colors.blue,
+        },
+      }
+    end
   },
   custom_highlights = {},
   integrations = {
@@ -69,7 +313,9 @@ require("catppuccin").setup({
         background = true,
       },
     },
-    neogit = true,
+    -- This setting seems to let me use my NeoGit highlight overrides, defined
+    -- above in `highlight_overrides`.
+    neogit = {},
     nvimtree = true,
     treesitter = true,
     treesitter_context = true,
