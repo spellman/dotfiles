975073608b58776f34e851d82b212f1b60beeded
Cort Spellman <cort@Wiecks-MacBook-Pro.local>
2024-06-03 20:33:16 -0500

    Source sexp.lua with relative path
    The absolute path was not necessarily portable between machines.

--- .config/nvim/after/plugin/sexp.lua
+++ .config/nvim/after/plugin/sexp.lua
@@ -1,2 +1,2 @@
 -- Can I use a relative path here?
-vim.cmd("source /Users/cort/Projects/dotfiles/.config/nvim/after/plugin/sexp_mappings_for_cort.vim")
+vim.cmd("source ${HOME}/Projects/dotfiles/.config/nvim/after/plugin/sexp_mappings_for_cort.vim")
