e33b4b535dac13b9647095db1cd15e3ad69d9e65
Cort Spellman <cort@wieck.com>
2024-08-26 08:07:41 -0500

    Nvim: Change Gitsigns markers

--- .config/nvim/lua/cort/plugins.lua
+++ .config/nvim/lua/cort/plugins.lua
@@ -81,16 +81,6 @@ return {
   {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
     "lewis6991/gitsigns.nvim",
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-    },
   },
 
   {
